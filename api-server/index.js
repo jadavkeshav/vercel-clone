793c697d1648d8e9144a655dceb89905251afd66
jadavkeshav <jadavkeshav2005@gmail.com>
2025-06-04 20:33:57 +0530

    [Api-Server] : Logs subsciber and /projects endpoint

--- api-server/index.js
+++ api-server/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const { generateSlug } = require("random-word-slugs");
 const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
+const Redis = require("ioredis");
+const { Server } = require("socket.io")
 require('dotenv').config();
 
+const subscriber = new Redis(process.env.REDIS_URL);
+
+const io = new Server({ cors: '*' });
+
+io.listen(9001, () => {
+    console.log("Socket.IO Server Running On PORT : 9001");
+});
+
+io.on("connection", (socket) => {
+    socket.on("subscribe", (channel) => {
+        // console.log(`Client subscribed to channel: ${channel}`);
+        socket.join(channel);
+        socket.emit("message", `Subscribed to channel: ${channel}`);
+    });
+
+});
+
 const ecs = new ECSClient({
-    region: "us-east-1",
+    region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
@@ -12,8 +31,8 @@ const ecs = new ECSClient({
 });
 
 const config = {
-    CLUSTER: '',
-    TASK: ''
+    CLUSTER: process.env.AWS_ECS_CLUSTER,
+    TASK: process.env.AWS_ECS_TASK_DEFINITION
 }
 
 const app = express();
@@ -34,14 +53,14 @@ app.post("/projects", async (req, res) => {
         networkConfiguration: {
             awsvpcConfiguration: {
                 assignPublicIp: 'ENABLED',
-                subnets: [''],
-                securityGroups: [''],
+                subnets: [process.env.AWS_ECS_CLUSTER_SUBNETS],
+                securityGroups: [process.env.AWS_ECS_CLUSTER_SECURITY_GROUP],
             }
         },
         overrides: {
             containerOverrides: [
                 {
-                    name: '',
+                    name: process.env.AWS_ECS_IMAGE_NAME,
                     environment: [
                         { name: 'GIT_REPOSITORY_URL', value: gitURL },
                         { name: 'PROJECT_ID', value: projectSlug }
@@ -52,7 +71,18 @@ app.post("/projects", async (req, res) => {
     });
 
     await ecs.send(command);
-    return res.json({ status : 'queued', data : {projectSlug, url: `http://${projectSlug}.localhost:8000`} });
+    return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } });
 })
+
+async function initRedisSubscribe(){
+    console.log("Subscribed To Logs Channel");
+    subscriber.psubscribe("logs:*");
+    subscriber.on("pmessage", (pattern, channel, message) => {
+        console.log("Redis Channel:", channel);
+        io.to(channel).emit("message", message);
+    });
+}
+
+initRedisSubscribe();
 
 app.listen(PORT, () => console.log(`API SERVER Running On PORT : ${PORT}`));
