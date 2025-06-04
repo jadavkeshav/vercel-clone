c38c25465bd58d34c2ba646919bb4a631cdae528
jadavkeshav <jadavkeshav2005@gmail.com>
2025-06-04 19:53:13 +0530

    Build-Server : Publishing Logs

--- build-server/script.js
+++ build-server/script.js
@@ -3,7 +3,11 @@ const path = require("path");
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
+const Redis = require("ioredis");
 require('dotenv').config();
+
+//create Redis client
+const publisher = new Redis(process.env.REDIS_URL);
 
 //create an S3 client
 const s3 = new S3Client({
@@ -16,9 +20,13 @@ const s3 = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
+function publishLogs(log) {
+    publisher.publish(`LOGS : ${PROJECT_ID}`, JSON.stringify({ log }));
+}
+
 async function init() {
     console.log("Executing Script.js");
-
+    publishLogs("Build Started...");
     const outDirPath = path.join(__dirname, "output");
     console.log("Output Directory Path: ", outDirPath);
 
@@ -28,19 +36,22 @@ async function init() {
     //when the command is running we can see the output in the console
     p.stdout.on("data", (data) => {
         console.log(data.toString());
+        publishLogs(data.toString());
     });
 
     p.stdout.on("error", (err) => {
         console.error("Error:", err.toString());
+        publishLogs(`ERROR: ${err.toString()}`);
     });
 
     p.on("close", async () => {
         console.log(`Build Complete.`);
-
+        publishLogs("Build Complete...");
         //read the dist floder after the build is complete
         const distFolderPath = path.join(__dirname, "output", "dist");
         const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true });
 
+        publishLogs("Starting To Upload...");
         //upload each file in the dist folder to S3
         for (const file of distFolderContents) {
             const filePath = path.join(distFolderPath, file);
@@ -48,7 +59,8 @@ async function init() {
                 continue;
             }
 
-            console.log("Uploading file: ", filePath);
+            console.log("Uploading file: ", file);
+            publishLogs(`Uploading file: ${file}`);
 
             const command = new PutObjectCommand({
                 Bucket: 'vc-build-server',
@@ -57,10 +69,11 @@ async function init() {
                 ContentType: mime.lookup(filePath),
             });
             await s3.send(command);
-            console.log("uploaded file: ", filePath);
-
+            console.log("uploaded file: ", file);
+            publishLogs(`uploaded file: ${file}`);
         }
         console.log("Done....");
+        publishLogs("Done...");
     });
 
 }
